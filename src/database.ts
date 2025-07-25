b279f20d99d5f90de0225d382e2983da853093d5
Gaston <gmanzano12@gmail.com>
2025-07-25 13:52:19 -0300

    .

--- src/database.ts
+++ src/database.ts
@@ -2,12 +2,11 @@ import * as dotenv from 'dotenv';
 import mongoose, { ConnectOptions } from 'mongoose';
 import * as path from 'path';
 
-dotenv.config({ path: path.join(__dirname, '/.env') });
+dotenv.config();
 mongoose.set('strictQuery', true);
 
 const { DATABASE_PASSWORD, DATABASE_USERNAME, DATABASE_NAME } = process.env;
 
-//const url = `mongodb://localhost:27017`
 const url = `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_NAME}.a1jzuce.mongodb.net/?retryWrites=true&w=majority&appName=Skere`;
 const connectDB = async (): Promise<void> => {
   await mongoose
